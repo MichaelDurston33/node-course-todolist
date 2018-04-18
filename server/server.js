3608d87444bd77b89362565f0660259ba5ec34ad
Michael Durston <michaeldurston@michaels-air.home>
2018-04-18 15:32:44 +0100

    added folder

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -25,31 +25,32 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos})
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
+
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      return res.status(404).send()
+      return res.status(404).send();
     }
-    res.status(200).send({todo});
+
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
-  })
+  });
 });
 
-
-
 app.listen(port, () => {
-  console.log(`Started on port ${port}`)
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
